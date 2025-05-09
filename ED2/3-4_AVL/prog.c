043ef2562a584fdb5779759d11033abce288591b
Melissa Moll <moll@alunos.utfpr.edu.br>
2025-05-09 10:40:55 -0300

    FIX: Configurando prog.c para o exercício 3

--- ED2/3-4_AVL/prog.c
+++ ED2/3-4_AVL/prog.c
@@ -4,18 +4,27 @@ int main () {
 
    Arvore *AVL = NULL;
 
-   AVL = inserir (AVL, 'Q');
-   /*TERMINAR (INSERIR DEMAIS CHAVES)!!!*/
-
-   //AVL = remover (AVL, 'A');
-   //AVL = remover (AVL, 'H');
-   //AVL = remover (AVL, 'E');
-   //AVL = remover (AVL, 'W');
-   //AVL = remover (AVL, 'G');
-   //AVL = remover (AVL, 'N');
-   //AVL = remover (AVL, 'P');
-   //AVL = remover (AVL, 'O');
-
+   AVL = inserir (AVL, "Pedro");
+   AVL = inserir (AVL, "Joaquim");
+   AVL = inserir (AVL, "Sandra");
+   AVL = inserir (AVL, "Daniele");
+   AVL = inserir (AVL, "Jhony");
+   AVL = inserir (AVL, "Bruna");
+   AVL = inserir (AVL, "Roberto");
+   AVL = inserir (AVL, "Ricardo");
+   AVL = inserir (AVL, "Ariane");
+   AVL = inserir (AVL, "Sara");
+   AVL = inserir (AVL, "Teodora");
+   AVL = inserir (AVL, "Rafael");
+   AVL = inserir (AVL, "Hayana");
+   AVL = inserir (AVL, "Lorena");
+   AVL = inserir (AVL, "Filonema");
+   AVL = inserir (AVL, "Mariana");
+   AVL = inserir (AVL, "Marina");
+   AVL = inserir (AVL, "Roberta");
+   AVL = inserir (AVL, "Chayene");
+   AVL = inserir (AVL, "Edmundo");
+   
    imprimir_in_order (AVL, 0);
 
    return 0;
